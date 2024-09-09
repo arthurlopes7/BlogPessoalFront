8f0b6fa869843344a5cd61b0b12ea4d6ce30bcba
arthurlopes7 <lopesarthur2013@gmail.com>
2024-09-08 23:29:56 -0300

    Adionado Cadastro e Login

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import Cadastro from "./pages/cadastro/Cadastro"
+import Login from "./pages/login/Login"
 
 function App() {
   return (
@@ -10,8 +12,11 @@ function App() {
         <Navbar />
         <div className="min-h-[80vh]">
           <Routes>
+          <Route path="/" element={<Cadastro />} />
             <Route path="/" element={<Home title="Home" />} />
             <Route path="/home" element={<Home title="Home" />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
         <Footer />
